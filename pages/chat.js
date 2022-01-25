8151e0226642cbc44d70706926ff1cd3d125ebb1
Vinicius Pereira <Vinicius.Pereira@processor.com.br>
2022-01-25 13:31:40 -0300

    Adicionando arquivos

---
+++ pages/chat.js
@@ -0,0 +1,13 @@
+export default function PaginaDoChat(){
+    return(
+        <>
+        
+        
+            <h1>Página do Chat</h1>
+        
+        
+        
+        
+        </>
+    )
+}
